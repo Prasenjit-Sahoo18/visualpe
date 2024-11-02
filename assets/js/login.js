b1f57644f6275cfe7d7f5bf842f8625d37892815
Ankush Saha <ankushsaha806@gmail.com>
2024-11-02 10:41:10 +0530

    bug fixes

--- assets/js/login.js
+++ assets/js/login.js
@@ -14,29 +14,29 @@ $(()=>{
 })
    
 const terms=()=> $("#btn").get(0).disabled=!$("#tc").get(0).checked | pin.length!=4 | $("#phn").get(0).value >1e10 | $("#phn").get(0).value <1e9
-const login=()=>{
-    $(sub).get(0).innerHTML=`<img src="assets/images/Pendulum.gif"><h3>loading ...</h3>`
-    fetch(`https://bridge-test-api.herokuapp.com/consent/${$("#phn").get(0).value}`,
-    {
-        method:'post',
-        mode:'cors',
+const login = () => {
+    $("#sub").get(0).innerHTML = `<img src="assets/images/Pendulum.gif"><h3>loading ...</h3>`;
+    fetch(`https://bridge-test-api.herokuapp.com/consent/${$("#phn").get(0).value}`, {
+        method: 'post',
+        mode: 'cors',
         credentials: 'same-origin',
         headers: {"Content-type": "application/json; charset=UTF-8"},
-        body:JSON.stringify({"pin":pin})
-    }
-    ).then((resp)=>resp.json())
-    .then((resp)=>{
-            {
-               console.log(resp)
-               localStorage.setItem("jwt",resp.token)
-               window.location.href=resp.url
-            }
-        })
-    .catch(()=>{ 
-        window.location.href="https://ankushsaha18.github.io/VisualPe/index.html"
+        body: JSON.stringify({"pin": pin})
     })
-       
+    .then((resp) => resp.json())
+    .then((resp) => {
+        localStorage.setItem("jwt", resp.token);
+        window.location.href = resp.url;
+    })
+    .catch(() => { 
+        window.location.href = "index.html";
+    });
 }
+
+// Make functions available globally if needed
+window.login = login;
+window.terms = terms;
+
 
 var jwt=localStorage.getItem("jwt")
 fetch(`https://bridge-test-api.herokuapp.com/checklogin`,
